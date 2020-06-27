24ab48209da015417fa67e041ae437721c16267b
Aniket Soni <aasoni1498@muleriders.saumag.edu>
2020-06-27 14:21:04 -0400

    modified code
    modified code

--- flash/settings.py
+++ flash/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((ea5eq%^6t!=_x2z9mb!u1)+f7h7l^q%423+e4n&io9nh2im^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
